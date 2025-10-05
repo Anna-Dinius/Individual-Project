8069aea53e655d1b4d38aa3754a46f175ef5d5df
Anna-Dinius <ae.dinius03@gmail.com>
2025-10-05 12:14:15 -0400

    added new data to firestore to test a restaurant that is not safe for tree nut allergies

--- firestore_uploader/firestore_uploader.js
+++ firestore_uploader/firestore_uploader.js
@@ -1,4 +1,7 @@
-/** Upload data from json documents to Firestore db */
+/**
+ * Upload data from json documents to Firestore db
+ * Use: node firestore_uploader/firestore_uploader.js
+ * */
 
 const admin = require('firebase-admin');
 
@@ -9,28 +12,28 @@ admin.initializeApp({
 const db = admin.firestore();
 
 // Restaurant data
-const restaurant_data = require('./data/restaurants.json');
+const restaurant_data = require('./new_data/restaurants.json');
 
 restaurant_data.forEach((restaurant) => {
 	db.collection('restaurants').doc(restaurant.id).set(restaurant);
 });
 
 // Address Data
-const address_data = require('./data/addresses.json');
+const address_data = require('./new_data/addresses.json');
 
 address_data.forEach((address) => {
 	db.collection('addresses').doc(address.id).set(address);
 });
 
 // Menu data
-const menu_data = require('./data/menus.json');
+const menu_data = require('./new_data/menus.json');
 
 menu_data.forEach((menu) => {
 	db.collection('menus').doc(menu.id).set(menu);
 });
 
 // Menu Item data
-const menu_items_data = require('./data/menu_items.json');
+const menu_items_data = require('./new_data/menu_items.json');
 
 menu_items_data.forEach((item) => {
 	db.collection('menu_items').doc(item.id).set(item);
