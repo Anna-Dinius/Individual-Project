09a0c9405ccc7d7f5e590caf5a5ee1b75bf676c2
Anna-Dinius <ae.dinius03@gmail.com>
2025-10-05 12:17:08 -0400

    moved allergen service functions from allergen.dart to allergen_service.dart

--- nomnom_safe/lib/models/entities/allergen.dart
+++ nomnom_safe/lib/models/entities/allergen.dart
@@ -1,5 +1,3 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
 class Allergen {
   final String id;
   final String label;
@@ -8,24 +6,5 @@ class Allergen {
 
   factory Allergen.fromJson(String id, Map<String, dynamic> json) {
     return Allergen(id: id, label: json['label']);
-  }
-
-  // TODO: move to service
-  Future<List<String>> getAllergenIds() async {
-    final snapshot = await FirebaseFirestore.instance
-        .collection('allergens')
-        .get();
-    return snapshot.docs.map((doc) => doc.id).toList();
-  }
-
-  // TODO: move to service
-  Future<List<Allergen>> getAllergens() async {
-    final snapshot = await FirebaseFirestore.instance
-        .collection('allergens')
-        .get();
-    return snapshot.docs.map((doc) {
-      final data = doc.data() as Map<String, dynamic>;
-      return Allergen.fromJson(doc.id, data);
-    }).toList();
   }
 }
